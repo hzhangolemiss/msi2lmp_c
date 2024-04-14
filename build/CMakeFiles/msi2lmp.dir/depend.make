56b447e0f287b74f5c78a06b1decc90c5350d005
Huadian Zhang <hzhang10@go.olemiss.edu>
2024-04-13 21:03:54 -0500

    changed somethings

--- build/CMakeFiles/msi2lmp.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for msi2lmp.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hzhang/Documents/msi2lmp_c
-# Build directory: /home/hzhang/Documents/msi2lmp_c/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
